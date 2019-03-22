83eeb442dda5a67a345cad8785a9df4c79315820
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-03-22 18:59:38 -0400

    *main.js +settings.json

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-const Discord = require("discord.js");
-const client = new Discord.Client
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-client.on(Ready);{
-    console.log(`Logged in as ${client.user}`);
-};
-client.on(Message)
+client.on('ready',()=>{
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message',msg=>{
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login('token');
