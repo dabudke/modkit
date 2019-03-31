712947948bd260fbeadbc116ff1a809df00c5aa0
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-03-30 21:54:01 -0400

    Changed some things

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require(config.json)
 
-client.on('ready',()=>{
+client.on('ready',() => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message',msg=>{
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+client.on('message',msg => {
+  if(msg.author != self) {
+    if(msg.content)
   }
 });
 
-client.login('token');
+client.login(config.token);
