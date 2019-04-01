99f707613bf77351284f1ea975f242cefc6c1af6
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-04-01 11:28:58 -0400

    Change Message Handler

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require(config.json)
+const config = require(config.json);
 
 client.on('ready',() => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message',msg => {
-  if(msg.author != self) {
-    if(msg.content.startsWith(config.prefix)) {
-      let args = message.content.slice(prefix.length).trim().split(/ +/g);
-      let command = args.shift().toLowerCase();
-      if (command = help) {
-        msg.channel.send(`Get ${msg.user.tag} some help!`)
-      } else {
-        msg.reply("Whoops, it looks like Ally doesn't have that command.  Use `a!help` for commands.")
-      }
-    }
-  }
+  if (msg.author.bot) return;
+  if (msg.content.startsWith(config.prefix)
 });
 
 client.login(config.token);
