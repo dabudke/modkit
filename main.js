c09809a9be6be42356951a7589d044a84fc8cb67
LittleKitacho <dbudke1010@outlook.com>
2019-04-09 20:03:40 -0400

    Changed a lot of things.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require(config.json);
+const token = require(token.json).token
 
 client.on('ready',() => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -8,8 +9,25 @@ client.on('ready',() => {
 
 client.on('message',msg => {
     if (msg.author.bot) { return; }
-    if (config.prefix !== msg.content.slice(0, config.prefix.len - 1)) { return; }
-
+    if (msg.content.indexOf(config.prefix) !== 0) { return; }
+    let args = msg.content.slice(config.prefix.len).trim().split(/ +/g);
+    let cmd = args.shift().toLowerCase();
+    switch(cmd) {
+      case "ping" :
+        msg.channel.send("Pong.  *Whaddaya expect me to do?*");
+        break;
+      case "pong" :
+        msg.channel.send("Ah, so you wanna turn the tables.  ***I see what you did there.***")
+        break;
+      case "say" :
+        if (msg.guild == false) {
+          msg.channel.send("Silly goose, you need to be in a server to do that!");
+        }
+        if (msg.guild.channels.indexOf(args[1]) !== 0) {
+          msg.channel.send("Oof, looks like that channel doesn't exist in this server")
+        }
+        if (msg.channel)
+    }
 });
 
-client.login(config.token);
+client.login(token);
