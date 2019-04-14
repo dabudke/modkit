a2e643e240645d4254c060344e1567b97d512f84
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-04-13 20:01:33 -0400

    Updated main.js

--- main.js
+++ main.js
@@ -2,42 +2,59 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require(config.json);
 const token = require(token.json).token;
+const global = null
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.author.bot) { return; }
-    if (msg.content.indexOf(config.prefix) !== 0) { return; }
+    if (msg.author.bot || msg.content.indexOf(config.prefix) !== 0) { return; }
     if (!msg.content.guild) {
         msg.channel.reply("Whaddaya doing, tryna slide me into DM's?  ***NO WAY JOSE.***");
+        return;
     }
-    let args = msg.content.slice(config.prefix.len).trim().split(/ +/g);
-    let cmd = args.shift().toLowerCase();
-    switch (cmd) {
-        case "ping":
-            msg.channel.send("Pong.  *Whaddaya expect me to do?*");
-            break;
-        case "pong":
-            msg.channel.send("Ah, so you wanna turn the tables.  ***I see what you did there.***");
-            break;
-        case "say":
-            if (!msg.guild) {
-                msg.channel.send("Silly goose, you need to be in a server to do that!");
-                break;
-            } else if (msg.guild.channels.indexOf(args[1]) === -1) {
-                msg.channel.send("Oof, looks like that channel doesn't exist in this server");
-                break;
-            } else if (msg.channel.memberPermissions(msg.author).indexOf("SEND_MESSAGES") === -1) {
-                msg.channel.send("Huh.  Guess I can't do that, \'cause you dont have chat permissions there.");
-                break;
+    const args = msg.content.slice(config.prefix.len).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if (cmd = "help") {
+        if (args[0] === "commands") {
+            const helpCommands = new Discord.RichEmbed()
+                .setTitle("Ally Help - Commands")
+                .setColor("#299FFF")
+                .addField("Admin")
+            msg.channel.send(helpEmbed)
+        } else {
+            const helpMaster = new Discord.RichEmbed()
+                .setTitle("Ally - Help")
+                .setColor("#299FFF")
+                .addField("Commands",`${config.prefix}help commands`)
+                .addField("Developers",`${config.prefix}help devs`)
+                .addField("Owner",`${config.prefix}help owner`)
+                .addField("GitHub Repository",`${config.prefix}help ghrepo`);
+            msg.channel.send(helpMaster);
+        }
+    } else if (cmd === "say") {
+        if (!msg.guild) {
+            msg.channel.send("Silly goose, you need to be in a server to do that!");
+        } else if (msg.guild.channels.indexOf(args[0]) === -1) {
+            msg.channel.send("Oof, looks like that channel doesn't exist in this server");
+        } else if (args[0].memberPermissions(msg.author).indexOf("SEND_MESSAGES") === -1) {
+            msg.channel.send("POLICE!  SOMEONE TRYNA CHAT WHERE THEY CAN'T");
+        } else {
+            if (args[2] === undefined || args[2] === false) {
+                const sayEmbed = new Discord.RichEmbed
+                    .setTitle(msg.author.username)
+                    .setDescription(args[1])
+                    .setAuthor(msg.author.tag, msg.author.avatarURL);
+            } else if (args[2] === true) {
+                args[0].send(args[1]);
             } else {
-                args[1].send(args[2]);
-                msg.channel.send("Message Sent.  **Let LittleKitacho#4507 know if an error occoured.**")
-            } break;
-        default:
-            msg.channel.send(`Sorry, that command doesn't exist.  Say \`${config.prefix}help\` for help.`);
+                msg.reply("On the last argument, use either 'true' or 'false'.")
+            }
+            msg.channel.send(`Sent ${args[1].toString} to channel ${args[0]}.`);
+        }
+    } else {
+        msg.channel.send("Well, can't help you there.  Don't have that command. Use `"+ config.prefix +"help commands` for a list of commands.")
     }
 });
 
