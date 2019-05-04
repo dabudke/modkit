1f783ce6f2b5cf69e548fca3fdecb292d860fd8b
LittleKitacho <dbudke1010@outlook.com>
2019-05-04 18:23:23 -0400

    Change message handler

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+// Main structure for discord.js.
 const Discord = require('discord.js');
 const client = new Discord.Client();
+// Assign files to variables
+const config = require('./config.json');
 
 client.on('ready',()=>{
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in at ${client.readyTimestamp} to ${client.usertag}.`);
 });
 
-client.on('message',msg=>{
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+client.on('message', msg => {
+    if (msg.content.startsWith(config.prefix)) {
+        if (msg.content === '/ping');
+    } else {
+        return;
+    }
 });
 
-client.login('token');
+client.login(config.token);
