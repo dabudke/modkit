9b694813088197eda9169d87979a55ffea3cd3aa
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-09-09 14:56:14 -0400

    Modify commands.

--- main.js
+++ main.js
@@ -17,32 +17,29 @@ client.on('message', msg => {
     const cmd = args.shift().toLowerCase();
     if (cmd = "help") {
         if (args[0] === "cmds" || args[0] === "commands") {
+            if (args[1] === "misc") {
+                msg.channel.send(new Discord.RichEmbed()
+                    .setTitle('Miscelaneous Commands')
+                    .setURL(config.website +'/commands/miscelaneous')
+                )
+            } else {
+                msg.channel.send(new Discord.RichEmbed()
+                    .setTitle("Command Directory")
+                    .setURL(config.website +'/commands')
+                )
+            }
+        } else {
             msg.channel.send(new Discord.RichEmbed()
-                .setTitle("Command Categories")
+                .setTitle('About Ally')
                 .setURL(config.website)
-                .setDescription('To see all commands in a category, use `$help cmds [category]`')
-                .addField('Utility',"$ping | $pong")
-                .addField('Fun',"$say")
-                .setAuthor(msg.author.tag, msg.author.tag)
+                .setDescription('Ally is a kind and humble Discord bot, written in Discord.js.')
+                .addField('Commands', 'Run `a?help commands`')
             )
-        } else {
-            msg.reply('');
         }
-    } else if (cmd === "say") {
-        if (!msg.guild.avalable) {
-            msg.channel.send("Oops, you need to be in a server for that!");
-        } else if (msg.guild.channels.indexOf(args[0]) === -1) {
-            msg.channel.send("Oops, that channel doesn't appear to exist.");
-        } else if (args[0].memberPermissions(msg.author).indexOf("SEND_MESSAGES") === -1) {
-            msg.channel.send("Oops, It looks like you can't talk there, so I can't on your behalf either.");
-        } else {
-           args[0].send(new Discord.RichEmbed()
-            .setTitle(args[1])
-            .setDescription(`Sent by ${msg.author} in ${msg.channel}`)
-            .setImage
-        );
-        msg.channel.send(`Sent ${args[1].toString} to channel ${args[0]}.`);
-        }
+    } else if (cmd === 'ping') {
+        msg.channel.send('Pong.')
+    } else if (cmd === 'pong') {
+        msg.channel.send('Ping?')
     } else {
         msg.channel.send(`It appears that I do not have that command.  For a complete list of commands, run \`${config.prefix}help\``)
     }
