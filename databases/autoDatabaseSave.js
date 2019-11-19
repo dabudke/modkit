4848516a471b6957aee041bd0eb80b13e5e7c208
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-11-19 17:00:12 -0500

    Change loop.

--- databases/autoDatabaseSave.js
+++ databases/autoDatabaseSave.js
@@ -2,7 +2,7 @@ const ms = require('./timeout.json')[1];
 const events = require('events');
 const emitter = new events.EventEmitter();
 
-while (true) {
+for (;;) {
     setTimeout(function() {
         emitter.emit('save')
     }, ms)
