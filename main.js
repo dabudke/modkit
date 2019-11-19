900da70d16dd07cf034fb273ea733e5c624e3e3e
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-11-19 16:14:49 -0500

    Do some mandatory eslint formatting.

--- main.js
+++ main.js
@@ -5,33 +5,32 @@ const bot = new Discord.Client();
 // Configuration Setup
 const fs = require("fs");
 const config = require('./config.json');
-const token = require(config.tokenRef).release;
+const token = require(config.tokenRef);
 let users = require(config.userDatabase);
 let guilds = require(config.guildDatabase);
 
 bot.on('ready', () => {
-    console.log(`Registered as ${bot.nick}`);
+    console.log(`Registered as ${bot.nick}`)
 });
 
 bot.on('message', (msg) => {
-    if (!msg.content.startsWith(config.prefix)) { return; };
-    if (msg.author.bot) { return; };
+    if (!msg.content.startsWith(config.prefix)) { return; }
+    if (msg.author.bot) { return; }
     let args = msg.content.slice(config.prefix.length()).split(" ").filter(Boolean),
         cmd = args.shift();
     if (cmd == "help") {
         if (args[1] == "cmds") {
-            var index = require(config.commandIndex)
+            var commands = require(config.commandIndex)
             var embed = new Discord.RichEmbed()
-                .setAuthor();
-            for (var i = 0; i <= (index.length() - 1); i++) {
-                embed.addField(index[i[1]], index[i[2]]);
+                .setTitle('Ally Commands')
+                .setDescription("Index of commands for Ally v"+config.prefix)
+                .setColor('#0096FF')
+                .setAuthor(msg.author.username, msg.author.avatarURL);
+            for (var i = 0; i <= commands.length(); i++) {
+                embed.addField(commands[i[1]], commands[i[2]]);
             }
-            msg.channel.send(embed);
-        } else {
-            var embed = new Discord.RichEmbed()
-                .setAuthor(msg.author.tag, msg.author)
             msg.channel.send(embed)
-        };
+        }
     }
 });
 
@@ -39,7 +38,7 @@ bot.on('message', (msg) => {
 const autoDatabaseSave = require('./databases/autoDatabaseSave');
 autoDatabaseSave.on('save', () => {
     fs.writeFileSync(config.userDatabase, JSON.stringify(users, null, 2));
-    fs.writeFileSync(config.userDatabase, JSON.stringify(users, null, 2));
+    fs.writeFileSync(config.guildDatabase, JSON.stringify(guilds, null, 2));
     console.log("Saved configuration.");
 });
 
