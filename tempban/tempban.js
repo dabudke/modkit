2c57352e336176e6b47379ddd51e386563fa27f1
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-11-21 16:16:56 -0500

    Add minutes, and localize everything.

--- tempban/tempban.js
+++ tempban/tempban.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const events = require('events');
 const emitter = new events.EventEmitter;
 let database = require("./database.json");
-const check = require('./unbanTimer.js');
 const date = new Date;
 let i, user;
 
@@ -17,16 +16,20 @@ exports.addBan = function(userID,year,month,day,hour) {
     fs.writeFileSync("./database.json", JSON.stringify(database, null, 2));
 };
 
-check.on('event', () => {
-    for (i = 0; i <= database.length(); i++) {
-        if (database[i].year == date.getFullYear()) {
-            if (database[i].month == date.getMonth()) {
-                if (database[i].day == date.getDate()) {
-                    if (database[i].hour == date.getHours()) {
-                        emitter.emit('unban', (database[i].id))
+for (;;) {
+    setTimeout(() => {
+        for (i = 0; i <= database.length(); i++) {
+            if (database[i].year >= date.getFullYear()) {
+                if (database[i].month >= date.getMonth()) {
+                    if (database[i].day >= date.getDate()) {
+                        if (database[i].hour >= date.getHours()) {
+                            if (database[i].minute >=date.getMinutes()) {
+                                emitter.emit('unban', (database[i].id));
+                            }
+                        }
                     }
                 }
             }
         }
-    }
-})
+    }, 60000);
+}
