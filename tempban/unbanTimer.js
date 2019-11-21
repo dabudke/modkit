03f31e115f0f36edd8ee3e9e08bb5402fc31108e
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-11-21 11:49:17 -0500

    Directly put timeout.

--- tempban/unbanTimer.js
+++ tempban/unbanTimer.js
@@ -1,9 +1,8 @@
 const events = require('events');
 const emitter = new events.EventEmitter;
-const timeout = require('timeout.json');
 
 for (;;) {
     setTimeout(() => {
         emitter.emit("event");
-    }, timeout);
+    }, 3600000);
 }
