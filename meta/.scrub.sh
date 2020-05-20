29bb1503fd827e4ca629ee762ed5e01574f4c7fb
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2020-05-20 17:49:59 -0400

    ignore the token file, since it is uploaded with placeholder values, plus remove the scrub applications since they are no longer needed.

--- meta/.scrub.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -f ./token.json
-echo "{" >> token.json
-echo "    \"token\": \"bot token\"," >> token.json
-echo "    \"testingToken\": \"testing bot token\"" >> token.json
-echo "}" >> token.json
