cc62f1c9ccbd5311f0ea5ede755cbfe286b56dd2
LittleKitacho <little.kitacho@outlook.com>
2020-05-20 15:15:55 -0400

    Add users.add() function, change database names, add default user object

--- databases/manager.js
+++ databases/manager.js
@@ -1,31 +1,36 @@
 const djs = require("discord.js");
 const fs = require("fs");
 
-const userDatabase = require("./users.json");
-const serverDatabase = require("./servers.json");
-const feedbackDatabase = require("./feedback.json");
+let userDb = require("./users.json");
+let serverDb = require("./servers.json");
+let feedbackDb = require("./feedback.json");
 
 setInterval( () => {
-    fs.writeFile( "./users.json", userDatabase );
-    fs.writeFile( "./servers.json", serverDatabase );
-    fs.writeFile( "./feedback.json", feedbackDatabase );
+    fs.writeFile( "./users.json", userDb );
+    fs.writeFile( "./servers.json", serverDb );
+    fs.writeFile( "./feedback.json", feedbackDb );
 }, 30000 );
 
 // user database functions
-exports.users = {}; // don't reference the database object here to keep integrity of database
-exports.users.addUser = ( userID, data ) => {};
-exports.users.modUser = ( userID, data ) => {};
-exports.users.remUser = ( userID ) => {};
+exports.users = {}; // don't reference the database object here to keep integrity of database// default user object
+exports.users.default = { points: { global: { level: 1, points: 0 } }, settings: { points: { announceGlobal: false, announceDM: true } } }
+exports.users.add = ( userId, data ) => {
+    let dataObj = { [userId]: exports.users.default };
+    userDb[userDb.length] = dataObj;
+    exports.users.modify( userId, data );
+};
+exports.users.modify = ( userId, data ) => {};
+exports.users.delete = userId => {};
 
 // server database functions
 exports.servers = {}; // don't reference the database object here to keep integrity of database
-exports.servers.addServer = ( guildID, data ) => {};
-exports.servers.modServer = ( guildID, data ) => {};
-exports.servers.remServer = ( guildID ) => {};
+exports.servers.add = ( guildId, data ) => {};
+exports.servers.mod = ( guildId, data ) => {};
+exports.servers.del = guildId => {};
 
 // feedback database function
-exports.feedback = {};
-exports.feedback.addFeedback = ( user, guild, data ) => {
+exports.feedback = {}; // don't reference the database object here to keep integrity of database
+exports.feedback.add = ( user, guild, data ) => {
     if (!user === djs.User) { return; };
     if (!guild === djs.Guild) { return; };
 
@@ -37,5 +42,5 @@ exports.feedback.addFeedback = ( user, guild, data ) => {
     dataObj.guild.id = guild.id;
     dataObj.guild.name = guild.id;
 
-    feedbackDatabase[feedbackDatabase.length] = dataObj;
+    feedbackDb[feedbackDb.length] = dataObj;
 };
