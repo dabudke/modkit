95d3c6bc934283385a404ea0124c59c6a6f6c095
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2020-06-02 22:53:56 -0400

    Fix some export errors, add presence export and set bot presence on login

--- .test.js
+++ .test.js
@@ -1,6 +1,7 @@
 console.log('You are running the test command: this will connect to the Discord API with the\
 \'testToken\' key in \'token.json\' file.')
 
-require("./src/main");
-
 exports.token = require("./meta/token.json").testToken;
+exports.presence = { activity: { name: "around with the fabric of Discord", type: "PLAYING"}, status: "dnd" };
+
+require("./src/main");
