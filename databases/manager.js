f8128f2cf3aaadcbafc8881496cd953c17b2c452
LittleKitacho <little.kitacho@outlook.com>
2020-07-26 18:08:55 -0400

    Change database manager so that scripts can directly infuence databases.

--- databases/manager.js
+++ databases/manager.js
@@ -1,11 +1,20 @@
+function writeDatabases() {
+    console.log("Writing databases, please wait...")
+    fs.writeFile( "./users.json", JSON.stringify( userDb, 2, null ) );
+    fs.writeFile( "./servers.json", JSON.stringify( serverDb, 2, null ) );
+    fs.writeFile( "./feedback.json", JSON.stringify( feedbackDb, 2, null ) );
+    console.log("Writing complete.")
+}
+
 const fs = require("fs");
 
 let userDb = require("./users.json");
 let serverDb = require("./servers.json");
 let feedbackDb = require("./feedback.json");
 
-setInterval( () => {
-    fs.writeFile( "./users.json", JSON.stringify( userDb, 2, null ) );
-    fs.writeFile( "./servers.json", JSON.stringify( serverDb, 2, null ) );
-    fs.writeFile( "./feedback.json", feedbackDb );
-}, 30000 );
+setInterval( writeDatabases() , 30000 );
+
+exports.writeDbs = writeDatabases;
+exports.userDb = userDb;
+exports.serverDb = serverDb;
+exports.feedbackDb = feedbackDb;
