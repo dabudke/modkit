abe754a0aa2defcd7a882d34ade2836b7f2ed434
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2020-07-27 17:39:40 -0400

    Change the presence a little

--- .test.js
+++ .test.js
@@ -2,6 +2,6 @@ console.log('You are running the test command: this will connect to the Discord 
 \'testToken\' key in \'token.json\' file.')
 
 exports.token = require("./meta/token.json").testToken;
-exports.presence = { activity: { name: "around with the fabric of Discord", type: "PLAYING"}, status: "dnd" };
+exports.presence = { activity: { name: "the latest code changes", type: "STREAMING"}, status: "online" };
 
 require("./src/main");
