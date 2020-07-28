4c280ec9154a56cb8b917b40b811bf55f4a57e59
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2020-07-28 11:21:15 -0400

    Change presence a little and fix a spelling mistake

--- .test.js
+++ .test.js
@@ -1,7 +1,7 @@
-console.log('You are running the test command: this will connect to the Discord API with the\
+console.log('You are running the test command: this will connect to the Discord API with the \
 \'testToken\' key in \'token.json\' file.')
 
 exports.token = require("./meta/token.json").testToken;
-exports.presence = { activity: { name: "the latest code changes", type: "STREAMING"}, status: "online" };
+exports.presence = { activity: { name: "for ^help", type: "WATCHING"}, status: "online" };
 
 require("./src/main");
