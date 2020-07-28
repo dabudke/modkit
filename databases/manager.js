436377121c09e6ae4cbff46558921ff2dade1c46
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2020-07-28 11:21:38 -0400

    Add functionality to only save certain databases

--- databases/manager.js
+++ databases/manager.js
@@ -1,9 +1,21 @@
-function writeDatabases() {
-    console.log("Writing databases, please wait...")
-    fs.writeFile( "./users.json", JSON.stringify( userDb, 2, null ) );
-    fs.writeFile( "./servers.json", JSON.stringify( serverDb, 2, null ) );
-    fs.writeFile( "./feedback.json", JSON.stringify( feedbackDb, 2, null ) );
-    console.log("Writing complete.")
+function writeDatabases(include) {
+    console.log("Writing databases, please wait...");
+    if ( include === null ) {
+        include = [ "users", "servers", "feedback" ];
+    }
+    for (i in include) {
+        switch(i) {
+            case "users":
+                fs.writeFile( "./users.json", JSON.stringify( userDb, 2, null ) );
+    
+            case "servers":
+                fs.writeFile( "./servers.json", JSON.stringify( serverDb, 2, null ) );
+    
+            case "feedback":
+                fs.writeFile( "./feedback.json", JSON.stringify( feedbackDb, 2, null ) );
+        }
+    }
+    console.log("Writing complete.");
 }
 
 const fs = require("fs");
@@ -14,7 +26,7 @@ let feedbackDb = require("./feedback.json");
 
 setInterval( writeDatabases() , 30000 );
 
-exports.writeDbs = writeDatabases;
+exports.writeDbs() = writeDatabases();
 exports.userDb = userDb;
 exports.serverDb = serverDb;
 exports.feedbackDb = feedbackDb;
