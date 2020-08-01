a5e70ff447973a7681619185eacdd5731434e648
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2020-07-31 20:15:09 -0400

    Fix database bug.

--- databases/manager.js
+++ databases/manager.js
@@ -1,9 +1,13 @@
 function writeDatabases() {
     console.log("Writing databases, please wait...");
+
+    fs.writeFileSync( "./users.json", JSON.stringify( userDb, 0, 2, null) );
+    fs.writeFileSync( "./servers.json", JSON.stringify( serverDb, 0, 2, null) );
+    fs.writeFileSync( "./feedback.json", JSON.stringify( feedbackDb, 0, 2, null) );
     
-    fs.writeFile( "./users.json", JSON.stringify( userDb, 2, null ) );
+    /*fs.writeFile( "./users.json", JSON.stringify( userDb, 2, null ) );
     fs.writeFile( "./servers.json", JSON.stringify( serverDb, 2, null ) );
-    fs.writeFile( "./feedback.json", JSON.stringify( feedbackDb, 2, null ) );
+    fs.writeFile( "./feedback.json", JSON.stringify( feedbackDb, 2, null ) );*/
     
     console.log("Writing complete.");
 }
@@ -14,7 +18,7 @@ let userDb = require("./users.json");
 let serverDb = require("./servers.json");
 let feedbackDb = require("./feedback.json");
 
-//setInterval( writeDatabases , 60000 );
+setInterval( writeDatabases , 60000 );
 
 //exports.writeDbs = () => { writeDatabases() };
 exports.userDb = userDb;
