99d25ebcda9d149968786f6aa0c3e1e88a785e29
LittleKitacho <little.kitacho@outlook.com>
2020-08-03 17:58:56 -0400

    Change template embed to be able to be copy/pasted without too many changes.

--- meta/embeds.js
+++ meta/embeds.js
@@ -1,51 +1,18 @@
 /* Template Embed:
 {
-    color: 0x0099ff,
-	title: 'Some title',
-	url: 'https://discord.js.org',
-	author: {
-		name: 'Some name',
-		icon_url: 'https://i.imgur.com/wSTFkRM.png',
-		url: 'https://discord.js.org',
-	},
-	description: 'Some description here',
+	title: ``,
+	description: ``,
+	url: "https://allydiscord.github.io/",
+	color: 0x0099FF,
 	thumbnail: {
-		url: 'https://i.imgur.com/wSTFkRM.png',
+		url: "https://imgur.com/YVRMcUD.png"
 	},
-	fields: [
-		{
-			name: 'Regular field title',
-			value: 'Some value here',
-		},
-		{
-			name: '\u200b',
-			value: '\u200b',
-			inline: false,
-		},
-		{
-			name: 'Inline field title',
-			value: 'Some value here',
-			inline: true,
-		},
-		{
-			name: 'Inline field title',
-			value: 'Some value here',
-			inline: true,
-		},
-		{
-			name: 'Inline field title',
-			value: 'Some value here',
-			inline: true,
-		},
-	],
-	image: {
-		url: 'https://i.imgur.com/wSTFkRM.png',
-	},
+	fields: [],
 	timestamp: new Date(),
 	footer: {
-		text: 'Some footer text here',
-		icon_url: 'https://i.imgur.com/wSTFkRM.png',
-	},
+		text: `Requested by {message author} | {bot name} v(non-production)`,
+		icon_url: {message profile picture link}
+	}
 }
 */
 const { prefix, name } = require("../meta/about.json");
