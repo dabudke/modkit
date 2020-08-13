0080f1fe4326e0420d6dad58538fddc1d23d70be
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2020-08-13 19:00:16 -0400

    Change if/else loop into switch/case.

--- src/main.js
+++ src/main.js
@@ -26,81 +26,106 @@ bot.on("message", (msg) => {
     const args = msg.content.split(/ +/gm);
     const cmd = args.shift().slice(about.prefix.length).toLowerCase();
 
-    if (cmd === "ping") {
-        msg.reply("pong!");
-    } else if ( cmd === "help" || cmd === "?") {
-        switch (args[0]) {
-            case "utility":
-                msg.channel.send({ embed: embeds.utility(msg) });
-                break;
+    // message handler
+    switch (cmd) {
+        case "ping":
+            msg.reply("pong!");
+            break;
 
-            case "moderation":
-                msg.channel.send({ embed: embeds.moderation(msg) });
-                break;
+        case "help":
+        case "?":
+            switch (args[0]) {
+                case "utility":
+                    msg.channel.send({ embed: embeds.utility(msg) });
+                    break;
 
-            case "warn":
-            case "!":
-                msg.channel.send({ embed: embeds.command.warn(msg) });
-                break;
+                case "moderation":
+                    msg.channel.send({ embed: embeds.moderation(msg) });
+                    break;
 
-            case "permissions":
-            case "perms":
-            case "perm":
-                msg.channel.send({ embed: embeds.command.permissions(msg) });
-                break;
-            
-            case "help":
-            case "?":
-                msg.channel.send({ embed: embeds.command.help(msg) });
-                break;
-            
-            case "about":
-                msg.channel.send({ embed: embeds.command.about(msg) });
-                break;
-            
-            case "settings":
-            case "setting":
-            case "sets":
-            case "set":
-                msg.channel.send({ embed: embeds.command.settings(msg) });
-                break;
+                case "warn":
+                case "!":
+                    msg.channel.send({ embed: embeds.command.warn(msg) });
+                    break;
 
-            case "feedback":
-            case "fb":
-                msg.channel.send({ embed: embeds.command.feedback(msg) });
-                break;
+                case "permissions":
+                case "perms":
+                case "perm":
+                    msg.channel.send({ embed: embeds.command.permissions(msg) });
+                    break;
+                
+                case "help":
+                case "?":
+                    msg.channel.send({ embed: embeds.command.help(msg) });
+                    break;
+                
+                case "about":
+                    msg.channel.send({ embed: embeds.command.about(msg) });
+                    break;
+                
+                case "settings":
+                case "setting":
+                case "sets":
+                case "set":
+                    msg.channel.send({ embed: embeds.command.settings(msg) });
+                    break;
 
-            case "ping":
-                msg.channel.send({ embed: embeds.command.ping(msg) });
-                break;
-            
-            default:
-                msg.channel.send({ embed: embeds.help(msg) });
-        }
-    }/* else if ( cmd === "about" ) {
-        //about command
-    }/* else if ( cmd === "settings" || cmd === "set" ) {
-        // settings command
-    }/* else if ( cmd === "warn" ) {
-        // warn command
-    }/* else if ( cmd === "permissions" || cmd === "perms" ) {
-        // permissions command
-    }*/ else if ( cmd === "feedback" || cmd === "fb") {
-        if ( args[0] === undefined ) {
-            msg.reply("you can submit feedback to the developers of Ally!  Put your feedback after `feedback` or `fb` to submit feedback.");
-        } else {
-            let feedback = args[0];
-            let i = false;
-            for (const arg in args) {
-                if (!i) { i = true; } else {
-                    feedback = feedback.concat(" ").concat(args[arg]);
-                }
+                case "feedback":
+                case "fb":
+                    msg.channel.send({ embed: embeds.command.feedback(msg) });
+                    break;
+
+                case "ping":
+                    msg.channel.send({ embed: embeds.command.ping(msg) });
+                    break;
+                
+                default:
+                    msg.channel.send({ embed: embeds.help(msg) });
             }
-            db.feedbackDb.push(feedback);
-            msg.reply("thanks for submitting feedback!  Your feedback is important, so keep submitting feedback as needed!");
-        }
-     } else {
-        msg.reply(`I didn't understand that command.  Please use \`${about.prefix}help\` to see all avaliable commands.`);
+            break;
+
+        case "about":
+            //about command
+            break;
+        
+        case "settings":
+        case "setting":
+        case "sets":
+        case "set":
+            // settings command
+            break;
+        
+        case "warn":
+        case "!":
+            // warn command
+            break;
+
+        case "permissions":
+        case "perms":
+        case "perm":
+            // permission command
+            break;
+
+        case "feedback":
+        case "fb":
+            if ( args[0] === undefined ) {
+                msg.reply("you can submit feedback to the developers of Ally!  Put your feedback after `feedback` or `fb` to submit feedback.");
+            } else {
+                let feedback = args[0];
+                let i = false;
+                for (const arg in args) {
+                    if (!i) { i = true; } else {
+                        feedback = feedback.concat(" ").concat(args[arg]);
+                    }
+                }
+                db.feedbackDb.push(feedback);
+                msg.reply("thanks for submitting feedback!  Your feedback is important, so keep submitting feedback as needed!");
+            }
+            break;
+
+        default:
+            msg.reply(`I didn't understand that command.  Please use \`${about.prefix}help\` to see all avaliable commands.`);
+            break;
     }
 });
 
