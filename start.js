fe02cd00d30b1fe34fa8b288f82e0a44191553a1
LittleKitacho <little.kitacho@outlook.com>
2020-10-24 16:25:25 -0400

    Change presence

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 console.log("Starting up the bot...");
 
 exports.token = require("./src/meta/token.json").token;
-exports.presence = { activity: { name: "the messages flow in through Discord", type: "WATCHING" }, status: "online" };
+exports.presence = { activity: { name: "the messages flow through | For help, use ^help", type: "WATCHING" }, status: "online" };
 
 console.log("Bot startup complete.  Main script will take over from this point.")
 
