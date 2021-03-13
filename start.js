1b442bd316cc540ac760970406c89f85a0ebdc55
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2021-03-13 17:36:50 -0500

    General cleanup and start work on TypeScript

--- start.js
+++ start.js
@@ -1,8 +1,11 @@
+const fs = require("fs");
+const token = fs.readFile("./token.txt", { encoding: "utf-8" });
+
 console.log("Starting up the bot...");
 
-exports.token = require("./src/meta/token.json").token;
+exports.token = token;
 exports.presence = { activity: { name: "the messages flow through | For help, use ^help", type: "WATCHING" }, status: "online" };
 
 console.log("Bot startup complete.  Main script will take over from this point.")
 
-require("./src/main");
+require("./js/main");
