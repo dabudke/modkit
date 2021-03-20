1e167c94ea3945a9e6e33b00fbffbdd97b7ff7ec
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2021-03-19 22:40:18 -0400

    Convert main script and about file to TS
    - Modify start.js to just call main script.

---
+++ src/meta/about.ts
@@ -0,0 +1,9 @@
+export const name = "Ally";
+export const version = "1.0.0-pre-1";
+export const team = {
+    projectLeads: ["LittleKitacho"],
+    developers: ["LittleKitacho"],
+    supportTeam: ["LittleKitacho"],
+    bugTrackers: []
+};
+export const prefix = "^";
