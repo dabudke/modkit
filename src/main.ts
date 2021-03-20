dcaa43d8472b7c1af58329bf5c4ebfbab03aef6c
LittleKitacho <little.kitacho@outlook.com>
2021-03-19 22:40:18 -0400

    Convert main script and about file to TS
    - Modify start.js to just call main script.

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+// get libraries and modules
+import * as Discord from "discord.js";
+import * as db from "./databases/manager";
+import * as commands from "./commands/handler";
+import { readFileSync } from "fs";
+import { prefix } from "./meta/about";
+
+// get meta files
+const token = readFileSync("../token.txt", { encoding: "utf-8" });
+
+// declare client
+const bot = new Discord.Client();
+
+bot.once("ready", () => {
+    console.log(`Logged in and connected to Discord (Username: ${bot.user.tag})`);
+    bot.user.setPresence( { status: "online", activity: { name: "people make bad life choices | For help, type ^help.", type: "WATCHING" } } );
+
+    bot.guilds.cache.forEach(guild => {
+        if (!db.getLocalGuild(guild.id)) {
+            db.createLocalGuild(guild.id);
+        }
+
+        /* TODO: initial guild join mock event */
+    });
+});
+
+bot.on("message", msg => commands.handle);
+
+bot.on("error", error => {
+    console.warn("An error occoured while communicating with Discord, here's what we got:\n\n", error);
+})
+
+bot.login(token).catch( error => {
+    console.error("There was a problem logging into Discord, most likely a bad token or no network connection.\nThis is what we got from Discord:\n\n", error);
+})
--- start.js
+++ start.js
@@ -1,11 +1,1 @@
-const fs = require("fs");
-const token = fs.readFileSync("./token.txt", { encoding: "utf-8" });
-
-console.log("Starting up the bot...");
-
-exports.token = token;
-exports.presence = { activity: { name: "the messages flow through | For help, use ^help", type: "WATCHING" }, status: "online" };
-
-console.log("Bot startup complete.  Main script will take over from this point.")
-
 require("./js/main");
