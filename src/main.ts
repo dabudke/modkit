6ad859ca98c3c5be68fb9b4e7100c56e455b942b
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2021-07-20 21:34:36 -0400

    Fix databases not loading properly
    So uhh, it turns out that I still had legacy code creating an empty Map, and was reading the wrong variable for the datatype.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { handle as handleCommand } from "./commands/handler";
 import { readFile } from "fs";
 import { prefix } from "./meta/config";
 import { updateLevel } from "./utils/levelManager";
+import { createLocalGuild, getLocalGuild } from "./databases/manager";
 
 // common types
 export type GuildId = string;
@@ -17,6 +18,12 @@ const bot = new Client();
 bot.once("ready", () => {
     console.log(`Logged in and connected to Discord (Username: ${bot.user.tag})`);
     bot.user.setPresence( { status: "online", activity: { name: "out for ^help", type: "WATCHING" } } );
+    bot.guilds.cache.forEach( guild => {
+        if (!getLocalGuild(guild.id)) {
+            // new guild!
+            bot.emit("guildCreate", guild);
+        }
+    });
 });
 
 bot.on("message", msg => {
@@ -29,6 +36,12 @@ bot.on("message", msg => {
 bot.on("error", error => {
     console.warn("An error occoured while communicating with Discord, here's what we got:\n\n", error);
 });
+
+bot.on("guildCreate", guild => {
+    // todo
+    createLocalGuild(guild.id);
+    console.log("New guild added!  " + guild.id)
+})
 
 readFile("./token.txt", "utf-8", ( err, token ) => {
     if (err) console.error("It appears that there is not a token.txt in the bot's root directory.  Please create the file, and paste your bot token into it.")
