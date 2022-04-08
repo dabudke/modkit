9f270d915f8f5bfd40fdb5cb3551e8fd97dce346
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2022-04-08 19:55:00 -0400

    Add user w/id strings

--- src/utils/userToString.ts
+++ src/utils/userToString.ts
@@ -4,14 +4,30 @@ export function username(user: User): string {
     return `**${user.username}**`;
 }
 
+export function usernameWithId(user: User): string {
+    return `**${user.username}** (${user.id})`;
+}
+
 export function usernameAndTag(user: User): string {
     return `**${user.tag}**`;
 }
 
+export function usernameAndTagWithId(user: User): string {
+    return `**${user.tag}** (${user.id})`;
+}
+
 export function displayName(user: GuildMember) {
-    return `**${user.displayName}`;
+    return `**${user.displayName}**`;
+}
+
+export function displayNameWithId(user: GuildMember) {
+    return `**${user.displayName}** (${user.id})`;
 }
 
 export function displaynameAndTag(user: GuildMember) {
     return user.nickname ? `**${user.nickname}** (${user.user.tag})` : `**${user.user.tag}**`;
+}
+
+export function displaynameAndTagWithId(user: GuildMember) {
+    return user.nickname ? `**${user.nickname}** (${user.user.tag} *${user.id}*)` : `**${user.user.tag}** (${user.id})`;
 }
